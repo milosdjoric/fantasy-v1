4efbc0a4fa2442fae3b97044b77dd04d9785fef8
djoric <djoric.inbox@gmail.com>
2017-01-02 23:24:42 +0100

    table gateway radi

--- module/Fantasy/src/Model/Fantasy.php
+++ module/Fantasy/src/Model/Fantasy.php
@@ -13,7 +13,13 @@ class Fantasy
     {
         $this->id = !empty($data['id']) ? $data['id'] : null;
         $this->player = !empty($data['player']) ? $data['player'] : null;
+        $this->team = !empty($data['team']) ? $data['team'] : null;
         $this->score = !empty($data['score']) ? $data['score'] : null;
+        $this->price = !empty($data['price']) ? $data['price'] : null;
+        $this->up = !empty($data['up']) ? $data['up'] : null;
+        $this->keeps_value = !empty($data['keeps_value']) ? $data['keeps_value'] : null;
+        $this->down = !empty($data['down']) ? $data['down'] : null;
+        $this->opponent = !empty($data['opponent']) ? $data['opponent'] : null;
     }
 
 }
